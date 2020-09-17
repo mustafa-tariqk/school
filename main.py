5567f4a8be1e70091e74cbb70cb1ebc1e2f4ba8b
Mustafa Tariq <58278308+mustafa-tariqk@users.noreply.github.com>
2020-09-17 15:24:13 +0000

    almost done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-name = input("Please enter your name: ")
-if name.isalpha():
-    print(name.upper())
-else:
-    print("Try again. ")
+running = True
+while running:
+    name = input("Please enter your name: ")
+
+    if name.isalpha():
+        print("Your name in all capital letters is", name.upper())
+
+        redo = input("Would you like to do this again: ")
+        if redo == "Yes":
+            running = True
+        elif redo == "No":
+            running = False
+
+    else:
+        print("Please try again and enter your name.")
+    
+    
