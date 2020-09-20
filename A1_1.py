34960da30f3c3c331548aff165914acd63c1aa80
Mustafa Tariq <58278308+mustafa-tariqk@users.noreply.github.com>
2020-09-20 16:22:52 +0000

    made things a lil neater

--- A1_1.py
+++ A1_1.py
@@ -4,8 +4,7 @@ Author: Mustafa Tariq
 Date:2020-09-30
 """
 
-running = True
-while running:  # Keeps running file until user choses to exit.
+while True:  # Keeps running file until user choses to exit.
     name = input("Please enter your first name: ")
 
     if name.isalpha():  # Checks if user input only contains a-z
@@ -13,7 +12,7 @@ while running:  # Keeps running file until user choses to exit.
 
         redo = input("Would you like to do this again: ")
         if redo != "Yes":  # Exits the program if input is not Yes.
-            running = False
+            break
             
     else:  # Asks user to input properly.
         print("Please try again and enter your name.")
