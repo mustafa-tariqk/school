6a5c3357e97003824339470e705e64db005c50eb
Mustafa Tariq <58278308+mustafa-tariqk@users.noreply.github.com>
2020-09-20 15:11:19 +0000

    documenting

--- A1_2.py
+++ A1_2.py
@@ -13,7 +13,7 @@ for friends in friendships:
     if friends[1] in friendships_dict:  # Appends values to dictionary.
         friendships_dict[friends[1]].append(friends[0])
 
-        if friends[0] in friendships_dict:
+        if friends[0] in friendships_dict:  # Looks at other value in 2D list.
             friendships_dict[friends[0]].append(friends[1])
         else:  # If a key is not in the dictionary it adds it.
             friendships_dict[friends[0]] = [friends[1]]
@@ -21,7 +21,7 @@ for friends in friendships:
     else:  # If a key is not in the dictionary it adds it.
         friendships_dict[friends[1]] = [friends[0]]
 
-        if friends[0] in friendships_dict:
+        if friends[0] in friendships_dict: 
             friendships_dict[friends[0]].append(friends[1])
         else:
             friendships_dict[friends[0]] = [friends[1]]
