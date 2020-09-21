c9613b634cafcba5da32b8f347370e6959ca58dc
Mustafa Tariq <58278308+mustafa-tariqk@users.noreply.github.com>
2020-09-21 11:04:06 -0400

    Update A1_1.py

--- A1_1.py
+++ A1_1.py
@@ -7,7 +7,7 @@ Date:2020-09-30
 while True:  # Keeps running file until user choses to exit.
     name = input("Please enter your first name: ")
 
-    if name.isalpha():  # Checks if user input only contains a-z
+    if name.isalpha():  # Checks if user input only contains a-z.
         print("Your name in all capital letters is %s." %name.upper())
 
         redo = input("Would you like to do this again: ")
