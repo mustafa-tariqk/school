d01d067f2fd13fede63707a8ba9dfb043455cc9d
Mustafa Tariq <58278308+mustafa-tariqk@users.noreply.github.com>
2020-10-05 18:58:35 +0000

    i think im almost done, ill peer review it

--- A2/A2.py
+++ A2/A2.py
@@ -1,21 +1,69 @@
+""" This program gets inputs, validates inputs
+and calculates outputs using fun_math.py.
+
+Author: Mustafa Tariq
+Student Number: 20212856
+Section: 001
+Date:2020-10-14
+"""
+
 import fun_math
 
+def valid_input(task):
+    """ Common for all functions to check input.
+    Use of ValueError as exception type.
+    """
+    while True: #  Keeps asking for input until valid.
+        try:
+            task()
+            break
+        except ValueError:
+            print("That is not a valid input, try again.\n")
+
+def task1():
+    """ Input and output for task 1.
+    """
+    base = int(input("Please enter a positive integer: "))
+    if base <= 0: #  Checks if integer is positive
+        raise ValueError
+    print(fun_math.cal_factorial(base), "\n")
+
+def task2():
+    """ Input and output for task 2.
+    """
+    number, length = map(int, input("Please enter a non-negative number and a length spaced: ").split())
+    if number < 0 or length <= 0: #  Checks if non-negative and positive.
+        raise ValueError
+    print(fun_math.list_multiples(number, length), "\n")
+
+def task3():
+    """ Input and output for task 3.
+    """
+    a_list = list(map(int,\
+             input("Please enter a list of spaced integers: ")\
+             .split())) #  Converts one line of spaced integers into a list.
+    print(fun_math.find_max(a_list), "\n")
+
 def main():
-    task = int(input("Please choose your task:\n"\
-                     "1 – calculate factorial\n"\
-                     "2 - generate a list of multiples\n"\
-                     "3 – find max number in a list\n"))
-    
-    if task == 1:
-        base = int(input("Please enter a positive integer: "))
-        print(fun_math.cal_factorial(base))
+    """ Function for exceution of program.
+    """
+    while True: #  Keeps asking for input until valid.
+        task = input("Please choose your task:\n"\
+                    "1 – calculate factorial\n"\
+                    "2 - generate a list of multiples\n"\
+                    "3 – find max number in a list\n"\
+                    "4 - exit the program\n")
 
-    elif task == 2:
-        number, length = input("Please enter a non-negative number and a length: ").split()
-        print(fun_math.list_multiples(int(number), int(length)))
+        #  Excecutes task based on input.
+        if task == "1":
+            valid_input(task1)
+        elif task == "2":
+            valid_input(task2)
+        elif task == "3":
+            valid_input(task3)
+        elif task == "4":
+            break
+        else:
+            print("That is not a valid option, try again.\n")
 
-    elif task == 3:
-        a_list = input("Please enter a list of integers: ")
-        print(fun_math.find_max(a_list.split()))
-        
 main()
--- A2/fun_math.py
+++ A2/fun_math.py
@@ -1,29 +1,65 @@
+""" This program contains functions that do some fun math.
+
+Author: Mustafa Tariq
+Student Number: 20212856
+Section: 001
+Date:2020-10-14
+"""
+
+
 def cal_factorial(x):
+    """ Returns factorial of an integer.
+    Calculates recursively.
+    """
     if x < 2:
         return 1
     else: 
         return x * cal_factorial(x-1)
 
 def list_multiples(number, length): 
+    """ Returns list of multiples.
+    List of multiples of number until length.
+    """
     multiples = []
     counter = 1
-    while counter <=length:
+    while counter <= length:
         multiples += [number*counter]
         counter += 1
     return multiples
 
 def find_max(a_list):
+    """ Finds the largest element in a list.
+    Searching done linearly.
+    """
     max = a_list[0]
-    for a in a_list[0:]:
-        if max < a:
-            max = a
+    for a_value in a_list:
+        if max < a_value:
+            max = a_value #  Changes value of max if another value is larger.
     return max
 
+if __name__ == "__main__": #  Testing cases for each function
 
-if __name__ == "__main__":
+    #  Test cases for cal_factorial(x)
+    print(cal_factorial(1))
+    print(cal_factorial(2))
     print(cal_factorial(4))
+    print(cal_factorial(8))
+    print(cal_factorial(16))
 
+    print()
+
+    #  Test cases for list_multiples(number, length)
     print(list_multiples(2,3))
+    print(list_multiples(3,2))
+    print(list_multiples(0,12))
     print(list_multiples(7,5))
+    print(list_multiples(5,7))
 
-    print(find_max([1,3,4,2,1,3,4,5,7,1,3,4,5,6,2,6,123,436,124,213]))
+    print()
+
+    #  Test cases for find_max(a_list)
+    print(find_max([1,3,4,2,1,3,4]))
+    print(find_max([54,234,63,12,35,32,213]))
+    print(find_max([431,74,243,36,3434,43]))
+    print(find_max([9,19,99,100]))
+    print(find_max([436,34,12,54,2]))
