f43665a40bb79e1a490799eace865959ff924385
Mustafa Tariq <58278308+mustafa-tariqk@users.noreply.github.com>
2020-10-20 18:27:31 +0000

    setting up some things

---
+++ Midterm/main.py
@@ -0,0 +1,15 @@
+""" This program contains functions that do some fun math.
+
+Author: Mustafa Tariq
+Student Number: 20212856
+Section: 001
+Date:2020-10-21
+"""
+
+
+def main():
+    """ Function for exceution of program.
+    """
+    print("Hello, world!")
+
+main()
